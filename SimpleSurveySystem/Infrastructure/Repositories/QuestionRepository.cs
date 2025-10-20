312dfce7e48395f2eff18846c20b7536657d2071
mohammad <mohammad@gmail.com>
2025-10-21 00:02:29 +0330

    Add Features

--- SimpleSurveySystem/Infrastructure/Repositories/QuestionRepository.cs
+++ SimpleSurveySystem/Infrastructure/Repositories/QuestionRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using SimpleSurveySystem.Contracts.RepositoryContracts;
 using SimpleSurveySystem.DTOs;
 using SimpleSurveySystem.Entities;
